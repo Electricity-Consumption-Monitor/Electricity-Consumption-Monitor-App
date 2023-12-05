889fb221a673cdcb03a48c85384977a99fad178a
Abdulraheem Bashaweeh <abashaweeh@stu.kau.edu.sa>
2023-12-05 23:28:41 +0300

    Get weather on WeatherService class. close #2

--- src/main/java/ElectricityMonitor/WeatherService.java
+++ src/main/java/ElectricityMonitor/WeatherService.java
@@ -1,7 +1,31 @@
 package ElectricityMonitor;
 
-
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 public class WeatherService {
 
+    private static final String API_KEY = "533e1baa54a8cf659fed8ec2b8a060f5";
+    private static final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric";
 
+    public static JsonObject getCurrentWeather(String city) {
+        String url = String.format(WEATHER_URL, city, API_KEY);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return JsonParser.parseString(response.body()).getAsJsonObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
 }
