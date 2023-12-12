51cf6a17a474cff48ae271408cd9dad898a74c65
Abdulraheem Bashaweeh <abashaweeh@stu.kau.edu.sa>
2023-12-13 02:35:59 +0300

    Added a Building test. close #14

--- Test/ElectricityMonitor/BuildingTest.java
+++ Test/ElectricityMonitor/BuildingTest.java
@@ -1,20 +1,53 @@
 package ElectricityMonitor;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BuildingTest {
 
     @Test
     public void testAddRoom() {
 
+        Building building = new Building("Test Building");
+
+        Room room1 = new Room();
+        Room room2 = new Room();
+
+        building.addRoom(room1);
+        building.addRoom(room2);
+
+        assertEquals(2, building.getRooms().size());
+        assertTrue(building.getRooms().contains(room1));
+        assertTrue(building.getRooms().contains(room2));
     }
 
     @Test
     public void testCalculateTotalElectricityBill() {
 
+        Building building = new Building("Test Building");
+
+        Room room1 = new Room();
+        Room room2 = new Room();
+
+        building.addRoom(room1);
+        building.addRoom(room2);
+
+        Device device1 = new Device("Device 1", 5.0, 100.0); // Provide hoursOfWork and wattsConsumed
+        Device device2 = new Device("Device 2", 5.0, 150.0);
+
+        room1.addDevice(device1);
+        room2.addDevice(device2);
+
+
+        double totalBill = building.calculateTotalElectricityBill();
+
+        assertEquals(45.0, totalBill, 0.001);
     }
     @Test
     public void testGetName() {
 
+        Building building = new Building("Test Building");
+        
+        assertEquals("Test Building", building.getName());
     }
 }
